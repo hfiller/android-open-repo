0b40058a8d7f33325bc44aa4c14940131d9a546f
Nick Reid <nreid26@uwo.ca>
2015-03-04 20:44:29 -0500

    -attempted location hook -added debugger support

---
+++ README.txt
@@ -0,0 +1,8 @@
+//////////
+/ Things /
+//////////
+
+JSConsole:
+	Open www.jsconsole.com in a browser
+	Enter ":listen dex-iteration-2"
+	Observe the magic
--- www/js/index.js
+++ www/js/index.js
@@ -1,22 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
+﻿var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -34,27 +16,67 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         alert("device ready");
+       
         app.receivedEvent('deviceready');
+        register()
+
     },
+
+    register: function() {
+           var pluginDef = {
+               "name": "AltostratusPlugin",
+               "namespace": "dexit.test.xKBplugin",
+               "extractDataFunction": "extract",
+               "pluginUpdatesFunction": "update"
+            }
+            dexit.device.sdk.xkbPluginManager.registerPlugin(pluginDef);
+
+        },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:done;');
+        receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-        //dexit.msdk.setPlayerUrl("player.dexit.co","/player");
-
-
-        //var parentElement = document.getElementById('playerFrame');
-
-
-
     }
 };
+dexit.test.xKBplugin.extract = function(args) {
 
-app.initialize();
+    console.log(" extract is passing")
+
+    var pos = navigator.geolocation.getCurrentPosition(
+          function(position) {
+              alert(position.coords.latitude + ',' + position.coords.longitude);
+              return position;
+          },
+          function() {
+              alert('Error getting location');
+          }
+    );
+
+    return {
+        position:pos,
+        time:(new Date().getTime())
+    };
+}
+
+dexit.test.xKBplugin.update = function(args){
+    var pos = navigator.geolocation.getCurrentPosition(
+        function(position) {
+            alert(position.coords.latitude + ',' + position.coords.longitude);
+            return position;
+        },
+        function() {
+            alert('Error getting location');
+        }
+    );
+
+    return {
+        position:pos,
+        time:(new Date().getTime())
+    };
+}​
