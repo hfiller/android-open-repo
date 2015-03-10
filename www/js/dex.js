009c73a2c9933aa624d5b0f02127dfdfea23bff8
Nick Reid <nreid26@uwo.ca>
2015-03-10 17:04:48 -0400

    reorganisaru

---
+++ www/js/dex.js
@@ -0,0 +1,83 @@
+﻿var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        console.log("device ready");
+       
+        app.receivedEvent('deviceready');
+        register()
+
+    },
+
+    register: function() {
+        var pluginDef = {
+            "name": "AltostratusPlugin",
+            "namespace": "dexit.test.xKBplugin",
+            "extractDataFunction": "extract",
+            "pluginUpdatesFunction": "update"
+        }
+        dexit.device.sdk.xkbPluginManager.registerPlugin(pluginDef);
+
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+dexit.test.xKBplugin.extract = function(args) {
+
+    console.log("extract is passing")
+
+    var pos = navigator.geolocation.getCurrentPosition(
+          function(position) {
+              console.log(position.coords.latitude + ',' + position.coords.longitude);
+              return position;
+          },
+          function() {
+              console.log('Error getting location');
+          }
+    );
+
+    return {
+        position:pos,
+        time:(new Date().getTime())
+    };
+};
+
+dexit.test.xKBplugin.update = function(args) {
+    var pos = navigator.geolocation.getCurrentPosition(
+        function(position) {
+            console.log(position.coords.latitude + ',' + position.coords.longitude);
+            return position;
+        },
+        function() {
+            console.log('Error getting location');
+        }
+    );
+
+    return {
+        position:pos,
+        time:(new Date().getTime())
+    };
+};
--- www/js/index.js
+++ www/js/index.js
@@ -1,83 +1,40 @@
-﻿var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        console.log("device ready");
-       
-        app.receivedEvent('deviceready');
-        register()
-
-    },
-
-    register: function() {
-        var pluginDef = {
-            "name": "AltostratusPlugin",
-            "namespace": "dexit.test.xKBplugin",
-            "extractDataFunction": "extract",
-            "pluginUpdatesFunction": "update"
-        }
-        dexit.device.sdk.xkbPluginManager.registerPlugin(pluginDef);
-
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+﻿var username = encodeURIComponent('hfiller2@uwo.ca');
+var password = encodeURIComponent('Lieske25113');
+$.ajax({
+    type:"POST",
+    url:"http://ice4e-developer.dexit.co/login?IDToken1="+username+"&IDToken2="+password+"&Login.Submit=Sign+In",
+    headers:{
+        'Content-Type': 'application/x-www-form-urlencoded'
     }
-};
+}).done(function(loggedIn){
+    console.log('something happened:');
+    console.log(loggedIn);
+})
+$.ajax({
+	type:"POST",
+	url:"https://sso.dexit.co/openam/oauth2/access_token?realm=altostratus&grant_type=password&username="+username+"&password="+password,
+	headers: {
+		'Authorization':'Basic ZHgtc2VydmljZToxMjMtNDU2LTc4OQ==',
+		'Content-Type' : 'application/x-www-form-urlencoded'
+	}
+}).done(function(tokenObject){
+	console.log('Token\n' + JSON.stringify(tokenObject));
 
-dexit.test.xKBplugin.extract = function(args) {
-
-    console.log("extract is passing")
-
-    var pos = navigator.geolocation.getCurrentPosition(
-          function(position) {
-              console.log(position.coords.latitude + ',' + position.coords.longitude);
-              return position;
-          },
-          function() {
-              console.log('Error getting location');
-          }
-    );
-
-    return {
-        position:pos,
-        time:(new Date().getTime())
-    };
-};
-
-dexit.test.xKBplugin.update = function(args) {
-    var pos = navigator.geolocation.getCurrentPosition(
-        function(position) {
-            console.log(position.coords.latitude + ',' + position.coords.longitude);
-            return position;
-        },
-        function() {
-            console.log('Error getting location');
-        }
-    );
-
-    return {
-        position:pos,
-        time:(new Date().getTime())
-    };
-};
+	$.ajax({
+		type:"POST",
+		url:"developer.kb.dexit.co/access/stores/course_admin/query?query=SELECT%20*%20FROM%20developer_course_student%20JOIN%20developer_course%20ON%20developer_course_student.course_id%20%3D%20developer_course.course_id%20AND%20student_email%3D%27"+username+"%27%3B",
+		headers:{
+			'Authorization':'Bearer ' + tokenObject.access_token
+		}
+	}).done(display(msg))
+});
+display(JSON.parse('{"result":{"headers":["course_id","student_email","grade","id","course_id","course_name","course_description","tenant"],"types":["VARCHAR","VARCHAR","VARCHAR","INT","VARCHAR","VARCHAR","VARCHAR","VARCHAR"],"rows":[["BI3245","sdownie@uwo.ca","96","2","BI3245","Business Intelligence","TBD","default"],["SE2245","sdownie@uwo.ca","99","3","SE2245","Internet Etchnology","TBD","default"]]}}'));
+function display(msg){
+    var rows = '<ul>'
+    for( var i in msg.result.rows){
+        //[0] courseId, [1] student, [2] course
+        rows += '<li><span >Course:<a class="course" href="http://player.dexit.co/player/?course='+msg.result.rows[i][0]+'">'+msg.result.rows[i][0]+'</a></span>  '+msg.result.rows[i][5]+'<span> Grade:'+msg.result.rows[i][2]+'</span><br><span id="'+msg.result.rows[i][0]+'Data">'+msg.result.rows[i][6]+'</span></li>';
+    }
+    rows += '</ul>';
+    $('#main').append(rows);
+}
